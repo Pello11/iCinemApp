5f28a9c0df1bbeecbd8a4e922856eb0f1145f075
spellons <spellons@gmail.com>
2023-01-25 15:41:38 +0100

    Initial Commit

---
+++ iCinemApp/MainTabBarViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iCinemApp
+//
+//  Created by Daniele Pellini on 25/01/23.
+//
+
+import UIKit
+
+class MainTabBarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
